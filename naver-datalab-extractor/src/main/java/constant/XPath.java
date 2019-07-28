65e11868f69065993f945b936a2ac8c1590b2e95
sungwonkim <sungwon5047@gmail.com>
2019-07-28 16:08:32 +0900

    네이버 데이터랩 검색 XPath 수정 on 2019.07.26

--- naver-datalab-extractor/src/main/java/constant/XPath.java
+++ naver-datalab-extractor/src/main/java/constant/XPath.java
@@ -16,9 +16,9 @@ public final class XPath {
     public static final String N_STRING_FORMAT_START_MONTH = "//*[@id=\"content\"]/div[2]/div/div[1]/div/div/div[2]/div[2]/span[1]/div[2]/ul/li/a[contains(text(),'%s')]";
     public static final String N_STRING_FORMAT_START_DAY = "//*[@id=\"content\"]/div[2]/div/div[1]/div/div/div[2]/div[2]/span[1]/div[3]/ul/li/a[contains(text(),'%s')]";
 
-    public static final String N_STRING_FORMAT_END_YEAR = "//*[@id=\"content\"]/div[2]/div/div[1]/div/div/div[2]/div[2]/span[1]/div[1]/ul/li/a[contains(text(),'%s')]";
-    public static final String N_STRING_FORMAT_END_MONTH = "//*[@id=\"content\"]/div[2]/div/div[1]/div/div/div[2]/div[2]/span[1]/div[2]/ul/li/a[contains(text(),'%s')]";
-    public static final String N_STRING_FORMAT_END_DAY = "//*[@id=\"content\"]/div[2]/div/div[1]/div/div/div[2]/div[2]/span[1]/div[3]/ul/li/a[contains(text(),'%s')]";
+    public static final String N_STRING_FORMAT_END_YEAR = "//*[@id=\"content\"]/div[2]/div/div[1]/div/div/div[2]/div[2]/span[3]/div[1]/ul/li/a[contains(text(),'%s')]";
+    public static final String N_STRING_FORMAT_END_MONTH = "//*[@id=\"content\"]/div[2]/div/div[1]/div/div/div[2]/div[2]/span[3]/div[2]/ul/li/a[contains(text(),'%s')]";
+    public static final String N_STRING_FORMAT_END_DAY = "//*[@id=\"content\"]/div[2]/div/div[1]/div/div/div[2]/div[2]/span[3]/div[3]/ul/li/a[contains(text(),'%s')]";
 
     public static final String N_SEARCH = "//*[@id=\"content\"]/div[2]/div/div[1]/div/a";
     public static final String N_NEXT_PAGE = "//*[@id=\"content\"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/a[2]";
