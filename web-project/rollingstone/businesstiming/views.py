33b4bb26ddcff03da45ecf238aa8f0c09591b3e3
kong <gnitsgogo@gmail.com>
2019-08-12 15:37:56 +0900

    django-init

--- web-project/rollingstone/businesstiming/views.py
+++ web-project/rollingstone/businesstiming/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'businesstiming/index.html')
+
+def keyword(request):
+    return render(request, 'businesstiming/keyword.html')
+
+def graph(request):
+    return render(request, 'businesstiming/graph.html')
